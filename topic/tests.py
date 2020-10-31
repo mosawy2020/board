c8dfb556a9c00367f44d3b3562260fba6c6ee62a
mosawy <sawyhawy@gmail.com>
2020-10-31 22:34:07 +0200

    first commit

---
+++ topic/tests.py
@@ -0,0 +1,49 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from .models import Post
+
+# Create your tests here.
+
+class BoardTests(TestCase):
+    def setUp(self):
+        self.user= get_user_model().objects.create_user(username="testuser" , email="test@gmail.com", password="secret")
+        self.post = Post.objects.create(title="test title", body="test body", author=self.user)
+
+    def test_string_representation(self):
+        post = Post(title="sample tilte")
+        self.assertEqual(str(post), post.title)
+
+    def test_post_content(self):
+        self.assertEqual(f"{self.post.title}", "test title")
+        self.assertEqual(f"{self.post.author}", "testuser")
+        self.assertEqual(f"{self.post.body}", "test body")
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(self.status_code, 200)
+        self.assertContains(response, "test body")
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_post_detail_view(self):
+        response=self.client.get("/post/1/")
+        no_response=self.client.get("/post/10000/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response,"test title")
+        self.assertTemplateUsed(response, "post_detail.html")
+
+    def test_post_create_view(self):
+        response= self.client.post(reverse("post_new"), {"title":"new title", "body":"new text", "author":self.user})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "new title")
+        self.assertContains(response, "new text")
+        
+    def test_post_update_view(self):
+        response= self.client.post(reverse("post_edit", args="1"), {"title":"update title", "body":"update text"})
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response= self.client.get(reverse("post_delete", args="1"))
+        self.assertEqual(response.status_code, 200)
+
---
+++ topic/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Post
+from django.urls import reverse_lazy
+# Create your views here.
+class TopicListView(ListView):
+    model = Post
+    template_name = "home.html"
+class TopicDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+class TopicCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = "__all__"
+class TopicUpdateView(UpdateView):
+    model = Post
+    template_name="post_edit.html"
+    fields=["title", "body"]
+class TopicDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
